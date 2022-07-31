0630d9029d1f9828348539d2a4420bde2d738cac
kana <lnbtkana2326@gmail.com>
2022-07-31 20:54:29 +0700

    edit _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,22 @@
 import Head from 'next/head'
 import Script from 'next/script'
+import '../styles/global.css'
 
 
 function MyApp({ Component, pageProps }) {
+  /* ไฟล์หลักของ app nextjs  */
   return (
     <>
       <Head>
           <meta charSet="UTF-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossOrigin="anonymous" />
-          <link href="https://fonts.googleapis.com/css2?family=Itim&family=Kanit&display=swap" rel="stylesheet" />
           <title>Barin Website</title>
       </Head>
       <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossOrigin="anonymous"></Script>
       <Component {...pageProps} />
     </>
-    
-    
   )
-  
-  
-  
 }
 
 export default MyApp
