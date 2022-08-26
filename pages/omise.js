f705edfecc3e0efb3c5ed6a60c56d383266584cb
Witnapat <lovelove6985@gmail.com>
2022-08-26 17:40:58 +0700

    test omise --- ref https://www.youtube.com/watch?v=gCJSFYvOu4c&list=PLCT_w0Fqe_z7JbLEBkco62V1Q1ZkSQ-7V&index=24

---
+++ pages/omise.js
@@ -0,0 +1,83 @@
+import React from "react";
+import Script from "react-load-script";
+
+let OmiseCard;
+
+console.log("pkey_test_5sxm3dmpgp2amagk9bh");
+const CheckoutWithCreditCard = ({ carts }) => {
+
+	const handleLoadScript = () => {
+		OmiseCard = window.OmiseCard;
+		OmiseCard.configure({
+			publicKey: "pkey_test_5sxm3dmpgp2amagk9bh",
+			currency: "thb",
+			frameLabel: "Tea Shop",
+			submitLabel: "PAY NOW",
+			buttonLabel: "Pay with Omise",
+		});
+	};
+
+	const creditCardConfigure = () => {
+		OmiseCard.configure({
+			defaultPaymentMethod: "credit_card",
+			otherPaymentMethods: [],
+		});
+		OmiseCard.configureButton("#credit-card");
+		OmiseCard.attach();
+	};
+
+	const omiseCardHandler = () => {
+		OmiseCard.open({
+			frameDescription: "Invoice #3847",
+			amount: 1500,
+      onCreateTokenSuccess: (token) => {
+        console.log("PAID");
+        console.log(token);
+				//   createCreditCardCharge(user.email, user.name, cart.amount, token)
+			},
+			onFormClosed: () => {},
+		});
+	};
+
+	const handleClick = () => {
+		creditCardConfigure();
+		omiseCardHandler();
+	};
+
+	return (
+		<div
+			style={{
+				width: "100%",
+				display: "flex",
+				justifyContent: "center",
+				alignItems: "center",
+				marginTop: "20px",
+			}}
+		>
+			<Script
+				url="https://cdn.omise.co/omise.js"
+				onLoad={handleLoadScript}
+			/>
+			<form>
+				<button
+					style={{
+						padding: "5px 10px",
+						cursor: "pointer",
+						background: "blue",
+						border: "none",
+						fontSize: "18px",
+						color: "white",
+					}}
+					id="credit-card"
+					type="button"
+					disabled={false}
+					onClick={handleClick}
+				>
+					Pay with Credit Card
+				</button>
+			</form>
+		</div>
+	);
+};
+
+export default CheckoutWithCreditCard;
