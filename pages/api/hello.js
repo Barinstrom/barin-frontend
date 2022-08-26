b0cdc209c9da31ce71a73ebf3329b2657363eb74
Witnapat <lovelove6985@gmail.com>
2022-08-27 00:34:58 +0700

    test omise vercel 11

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,10 +2,5 @@
 
 export default async function handler(req, res) {
 
-  var omise = require("omise")({
-		publicKey: "pkey_test_5sxm3dmpgp2amagk9bh",
-		secretKey: "skey_test_5sxlti3pxutrdqmhye3",
-	});
-
   res.status(200).json({ name: 'John Doe' })
 }
