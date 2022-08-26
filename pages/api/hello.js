58266bb83347c22c52bf50688869fd3fcd8821d3
Witnapat <lovelove6985@gmail.com>
2022-08-27 00:30:08 +0700

    test omise vercel 9

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default async function handler(req, res) {
+
+  var omise = require("omise")({
+		publicKey: "pkey_test_5sxm3dmpgp2amagk9bh",
+		secretKey: "skey_test_5sxlti3pxutrdqmhye3",
+	});
+
   res.status(200).json({ name: 'John Doe' })
 }
