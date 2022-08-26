f8d6db8d83829cc1398f6e862e4a166bf3eb3f16
Witnapat <lovelove6985@gmail.com>
2022-08-27 00:32:01 +0700

    test omise vercel 10

---
+++ pages/api/payment.js
@@ -0,0 +1,10 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default async function handler(req, res) {
+	var omise = require("omise")({
+		publicKey: "pkey_test_5sxm3dmpgp2amagk9bh",
+		secretKey: "skey_test_5sxlti3pxutrdqmhye3",
+	});
+
+	res.status(200).json({ name: "Payment Doe" });
+}
