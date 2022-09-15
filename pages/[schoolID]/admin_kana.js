d5935bff557356be9fb420e47cb025b461b6e642
kana <lnbtkana2326@gmail.com>
2022-09-15 18:50:10 +0700

    admin_school clear code and doing perfect

---
+++ pages/[schoolID]/admin_kana.js
@@ -0,0 +1,436 @@
+import React, { useEffect, useRef, useState } from "react";
+import Link from "next/link";
+import styles from "../../styles/admin.module.css";
+import { get_data } from "../../utils/auth";
+
+import EditStudent from "../../components/admin_school/editStudent";
+import EditTeacher from "../../components/admin_school/editTeacher";
+import EditClub from "../../components/admin_school/editClub";
+import EditOwnData from "../../components/admin_school/editOwnData";
+import InsertClub from "../../components/admin_school/insertClub";
+import InsertStudent from "../../components/admin_school/insertStudent";
+import InsertTeacher from "../../components/admin_school/insertTeacher";
+import SchoolData from "../../components/admin_school/schoolData";
+import TimeConfig from "../../components/admin_school/timeConfig";
+import Reload from "../../components/reload";
+import Cookies from "universal-cookie";
+import Error from "next/error";
+
+export default function Admin({ schoolID,school_data }) {
+	const nav = useRef();
+	const time = useRef();
+	const optionBtn = useRef([])
+	const hamberger = useRef()
+	let timer;
+
+	const [displayFirst,setDisplayFirst] = useState("loading")
+	const [data_school,setData_school] = useState()
+	const [readyTime,setReadyTime] = useState(false)
+	const [countBtn,SetCountBtn] = useState(0)
+	const [chooseBtnStart,setchooseBtnStart] = useState(false)
+
+	useEffect(() => {
+		if (chooseBtnStart){
+			optionBtn.current[0].classList.add("nowclick");
+			if (!school_data.paymentStatus) {
+				for (let i = 0; i < 9; i++) {
+					if (i !== 0) {
+						optionBtn.current[i].hidden = true
+					}
+				}
+			} 
+		}
+	},[chooseBtnStart])
+
+	useEffect(() => {
+		if (readyTime){
+			controllTime("start");
+			
+			return () => {
+				controllTime("cancell");
+			}
+		}
+	},[readyTime])
+
+	useEffect(() => {
+		const cookies = new Cookies();
+		const token = cookies.get("token");
+
+		Promise.all([get_data(token,schoolID)])
+		.then(result => {
+			const data_tmp = result[0]
+			if (data_tmp){
+				setDisplayFirst(true)
+				setData_school(data_tmp.data.data_school)
+				setchooseBtnStart(true)
+				setReadyTime(true)
+			}else{
+				setDisplayFirst(false)
+			}
+		})
+	},[])
+
+
+	function changeComponent(num) {
+		if (num == 0) {
+			SetCountBtn(0)
+		} else if (num == 1) {
+			SetCountBtn(1)
+		} else if (num == 2) {
+			SetCountBtn(2)
+		} else if (num == 3) {
+			SetCountBtn(3)
+		} else if (num == 4) {
+			SetCountBtn(4)
+		} else if (num == 5) {
+			SetCountBtn(5)
+		} else if (num == 6) {
+			SetCountBtn(6)
+		} else if (num == 7) {
+			SetCountBtn(7)
+		} else {
+			SetCountBtn(8)
+		}
+		
+		for (let i=0;i<=8;i++){
+			if (i === num){
+				optionBtn.current[i].classList.add("nowclick")
+			}else{
+				optionBtn.current[i].classList.remove("nowclick")
+			}
+		}
+		nav.current.classList.remove("active");
+		hamberger.current.classList.remove("hamactive");
+	}
+
+	/* ฟังชันก์ set เวลาให้นับแบบ real timer */
+	function controllTime(check) {
+		if (check == "start") {
+			timer = setInterval(() => {
+				const h = String(new Date().getHours()).padStart(2, "0");
+				const m = String(new Date().getMinutes()).padStart(2, "0");
+				const s = String(new Date().getSeconds()).padStart(2, "0");
+				time.current.innerText = `${h}:${m}:${s}`;
+			}, 1000);
+		} else {
+			clearInterval(timer);
+		}
+	}
+
+	let component = null
+	if (countBtn === 0){
+		component = <SchoolData school_data={data_school} />
+	}else if (countBtn === 1){
+		component = <TimeConfig school_data={data_school} />
+	}else if (countBtn === 2){
+		component = <InsertTeacher school_data={data_school} />
+	}else if (countBtn === 3){
+		component = <InsertStudent school_data={data_school} />
+	}else if (countBtn === 4){
+		component = <InsertClub school_data={data_school} />
+	}else if (countBtn === 5){
+		component = <EditStudent school_data={data_school} />
+	}else if (countBtn === 6){
+		component = <EditTeacher school_data={data_school} />
+	}else if (countBtn === 7){
+		component = <EditClub school_data={data_school} />
+	}else{
+		component = <EditOwnData school_data={data_school} />
+	}
+	
+	const clickHamberger = () => {
+		hamberger.current.classList.toggle("hamactive");
+		nav.current.classList.toggle("active");
+	};
+
+	const admin_page = (
+		<>
+			<style jsx>{`
+				.nav_header {
+					min-height: 100vh;
+					position: fixed;
+					padding: 3px;
+					transform: translate(-5%, 80px);
+					transition: transform 0.3s ease;
+					z-index: 100;
+					background-color: transparent;
+				}
+
+				.button_hamberger{
+					width: 40px;
+					height: 40px;
+					border: none;
+					opacity: 1;
+					border-radius: 15px;
+					font-size: 1.4rem;
+					margin-right: 10px;
+					display: none;
+					background-color: transparent;
+				}
+
+				@media screen and (max-width: 1300px) {
+					.nav_header {
+						transform: translate(-100%, 80px);
+					}
+					.nav_header.active {
+						transform: translateY(80px);
+						background-color: white;
+					}
+					.button_hamberger{
+						display: block;
+					}
+					.button_hamberger.hamactive{
+						background-color: #e8e8e8;
+					}
+				}
+
+				.h2_alert,
+				.h2_alert {
+					font-size: 36px;
+				}
+
+				@media screen and (max-width: 1000px) {
+					.h2_alert {
+						font-size: 28px;
+					}
+				}
+				@media screen and (max-width: 800px) {
+					.h2_alert {
+						font-size: 20px;
+					}
+				}
+
+				.nav_left{
+					text-align: left;
+					border-radius: 10px;
+					padding: 6px 30px;
+					cursor: pointer;
+				}
+
+				.nav_left.nowclick{
+					background-color: #FFFFFF;
+					box-shadow: rgba(0, 0, 0, 0.40) 2px 4px 10px;
+				}
+			`}</style>
+
+			<header className={`${styles.head} navbar navbar-dark bg-white`}>
+				<div className={`${styles.header_main} text-dark d-flex justify-content-between shadow`}>
+					<div className={`${styles.header_item} ms-2 `}>
+						<button
+							className="button_hamberger"
+							onClick={clickHamberger}
+							ref={hamberger}
+						>
+							<i className="fa-solid fa-bars"></i>
+						</button>
+						<span className="ms-3">Dashboard</span>
+					</div>
+					<div className={`${styles.header_item}`}>
+						<div className={`${styles.time_alert} me-2`}>
+							<span ref={time}></span>
+						</div>
+						<div className={`me-2`}>
+							<span className={`${styles.logo_bell}`}>
+								<i className="fa-regular fa-bell"></i>
+							</span>
+							<span className={`${styles.user_name} ms-1`}>
+								{/* {data.data.userId} */}
+							</span>
+							<Link href="/">
+								<a className={`${styles.logo} ms-2`}>
+									<img src={"../../dora.jpg"} />
+								</a>
+							</Link>
+						</div>
+					</div>
+				</div>
+			</header>
+
+			<nav className="nav_header" ref={nav}>
+ 				<div className={styles.box_menu}>
+					<ul>
+ 						<li>
+							<div className={`nav_left`} 
+								onClick={() => changeComponent(0)}
+								ref={(el) => optionBtn.current[0] = el}
+							>
+								<i className="fa-solid fa-house me-2"></i>
+								<span>ข้อมูลโรงเรียน</span>
+							</div>
+						</li>
+						<li>
+							<div className={`nav_left`} 
+								onClick={() => changeComponent(1)}
+								ref={(el) => optionBtn.current[1] = el}
+							>
+								<i className="fa-solid fa-calendar-days me-2 ms-1"></i>
+								<span >ตั้งเวลาลงทะเบียน	</span>
+							</div>
+						</li>
+						<li>
+							<div className={`nav_left`} 
+								onClick={() => changeComponent(2)}
+								ref={(el) => optionBtn.current[2] = el}
+							>
+								<i className="fa-solid fa-chalkboard-user "></i>
+								<span className="ms-2">เพิ่มข้อมูลครู</span>
+							</div>
+						</li>
+						<li>
+							<div className={`nav_left`} 
+								onClick={() => changeComponent(3)}
+								ref={(el) => optionBtn.current[3] = el}
+							>
+								<i className="fa-solid fa-chalkboard-user "></i>
+								<span className="ms-2">เพิ่มข้อมูลนักเรียน</span>
+							</div>
+						</li>
+						<li>
+							<div className={`nav_left`} 
+								onClick={() => changeComponent(4)}
+								ref={(el) => optionBtn.current[4] = el}
+							>
+								<i className="fa-solid fa-book me-2 ms-1"></i>
+								<span>เพิ่มข้อมูลชุมนุม</span>
+							</div>
+						</li>
+						<li>
+							<div className={`nav_left`} 
+								onClick={() => changeComponent(5)}
+								ref={(el) => optionBtn.current[5] = el}
+							>
+								<i className="fa-solid fa-list-check"></i>
+								<span className="ms-2">แก้ไขข้อมูลนักเรียน</span>
+							</div>
+						</li>
+						<li>
+							<div className={`nav_left`} 
+								onClick={() => changeComponent(6)}
+								ref={(el) => optionBtn.current[6] = el}
+							>
+								<i className="fa-solid fa-list-check"></i>
+								<span className="ms-2">แก้ไขข้อมูลครู</span>
+							</div>
+						</li>
+						<li>
+							<div className={`nav_left`} 
+								onClick={() => changeComponent(7)}
+								ref={(el) => optionBtn.current[7] = el}
+							>
+								<i className="fa-solid fa-list-check"></i>
+								<span className="ms-2">แก้ไขข้อมูลชุมนม</span>
+							</div>
+						</li>
+						<li>
+							<div className={`nav_left`} 
+								onClick={() => changeComponent(8)}
+								ref={(el) => optionBtn.current[8] = el}
+							>
+								<i className="fa-solid fa-list-check"></i>
+								<span className="ms-2">แก้ไขข้อมูลตัวเอง</span>
+							</div>
+						</li>
+					</ul>
+				</div>
+			</nav>
+
+			<main className={styles.content}>
+				<section className="container border">
+					{component}
+				</section>
+			</main>
+		</>
+	)
+
+	if (displayFirst === "loading") { 
+		return <Reload />
+	}
+	else if (displayFirst) {
+		return admin_page
+	}
+	else {
+		return <Error statusCode={404}/>
+	}
+}
+
+export async function getStaticPaths() {
+	// เอาไว้ดึง api กำหนด path
+	/* const response = await fetch("http://127.0.0.1:8000/user")
+		const data = await response.json()
+	  
+		const b = data.map((e) => {
+				return {params: {id : `${e.id}` }}
+		}) */
+
+	let a = [
+		{ params: { schoolID: "1" } },
+		{ params: { schoolID: "2" } },
+		{ params: { schoolID: "3" } },
+		{ params: { schoolID: "stamp" } },
+		{ params: { schoolID: "teststamp" } },
+	];
+
+	return {
+		paths: a,
+		fallback: false,
+	};
+}
+
+export async function getStaticProps(context) {
+	//console.log(context);
+	/*  const response = await fetch(`http://127.0.0.1:8000/user/${context.params.id}`)
+		const data = await response.json() */
+	// console.log("context", context);
+	const schoolID = context.params.schoolID
+	// "2020-09-02" = yyyy-mm-dd
+	const school_data = {
+		schoolName: "Stamp Witnapat School",
+		Status: "active",
+		paymentStatus: true, // จ่ายเงินหรือยัง
+		urlLogo: "https://upload.wikimedia.org/wikipedia/commons/a/a2/Prommanusorn.png",
+		urlDocument: "https://image.shutterstock.com/image-vector/vector-logo-school-260nw-427910128.jpg",
+		schoolID: context.params.schoolID,
+		nowSchoolYear: "2021",
+		schedule:[
+			// {
+			// 	// nowSchoolYear:true,
+			// 	schoolYear: "2022",
+			// 	registerDate: "",
+			// 	registerTime: "",
+			// 	endOfRegisterDate: "",
+			// 	endOfRegisterTime: "",
+			// 	endOfSchoolYear: "",
+			// },
+			{
+				// nowSchoolYear:false,
+				schoolYear: "2021",
+				registerDate: "2021-05-01",
+				registerTime: "10:00:00",
+				endOfRegisterDate: "2021-05-10",
+				endOfRegisterTime: "16:00:00",
+				endOfSchoolYear: "2021-10-13",
+			},
+			{
+				// nowSchoolYear:false,
+				schoolYear: "2020",
+				registerDate: "2020-05-01",
+				registerTime: "09:00:00",
+				endOfRegisterDate: "2020-05-10",
+				endOfRegisterTime: "16:00:00",
+				endOfSchoolYear: "2020-10-15",
+			},
+			{
+				// nowSchoolYear:false,
+				schoolYear: "2019",
+				registerDate: "2019-05-01",
+				registerTime: "10:00:00",
+				endOfRegisterDate: "2019-05-12",
+				endOfRegisterTime: "16:00:00",
+				endOfSchoolYear: "2019-10-12",
+			}]
+	}
+	return {
+		props: { schoolID,school_data },
+		revalidate: 1,
+	};
+}
